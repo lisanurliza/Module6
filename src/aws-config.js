ecb87a0bf578f77331a26d84b5b4a079694328ab
lisanurliza <norliza.abdullah23@gmail.com>
2024-10-02 16:44:12 +0800

    Delete src/aws-config.js

--- src/aws-config.js
+++
@@ -1,6 +0,0 @@
-// aws-config.js
-export const AWS_REGION = "ap-southeast-1";
-export const IDENTITY_POOL_ID =
-  "ap-southeast-1:3211b668-d076-4a72-a377-1933eaa868f2";
-export const AWS_IOT_ENDPOINT =
-  "agh6kxgv8jmup-ats.iot.ap-southeast-1.amazonaws.com";
